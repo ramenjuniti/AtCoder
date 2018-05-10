c748a3cbabcd6ac1d3459131197a597f4d389129
ramenjuniti <ramenjuniti@gmail.com>
2018-05-10 16:34:59 +0900

    abc0861b問題を解いた

--- beginnersSelection/abc086a.go
+++ beginnersSelection/abc086a.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var a, b int
 	fmt.Scanf("%d %d", &a, &b)
-	if(a*b % 2 == 0) {
+	if a*b%2 == 0 {
 		fmt.Printf("Even\n")
 	} else {
 		fmt.Printf("Odd\n")
--- practice/hajimete.go
+++ practice/hajimete.go
@@ -6,8 +6,8 @@ func main() {
 	var a, b, c int
 	var s string
 
-	fmt.Scanf("%d", &a);
-	fmt.Scanf("%d %d", &b, &c);
-	fmt.Scanf("%s", &s);
-	fmt.Printf("%d %s\n", a+b+c, s);
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d %d", &b, &c)
+	fmt.Scanf("%s", &s)
+	fmt.Printf("%d %s\n", a+b+c, s)
 }
